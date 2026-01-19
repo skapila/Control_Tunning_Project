300764be68584f40caa33e5108ac4a743b9d847f
skapila <samkapila95@gmail.com>
2026-01-19 14:28:09 +0530

    Delete FlightController directory

--- FlightController/launch_sitl.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Launch an application (replace with your app)
-APP_PATH=  sim_vehicle.py -D -v ArduCopter -f JSON --add-param-file=/home/samarth/Desktop/ardupilot_gazebo/config/gazebo-iris-gimbal.parm
-
-$APP_PATH 
-
