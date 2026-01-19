e1597c4c85e3b427c19818b7a9cd164e3a6af658
skapila <samkapila95@gmail.com>
2026-01-19 14:27:46 +0530

    Delete ArdupilotRcTraining directory

--- ArdupilotRcTraining/launch_sitl.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Launch an application (replace with your app)
-APP_PATH=  sim_vehicle.py -D -v ArduCopter -f JSON --add-param-file=/home/samarth/Desktop/ardupilot/Tools/autotest/default_params/gazebo-iris.parm
-$APP_PATH 
-
