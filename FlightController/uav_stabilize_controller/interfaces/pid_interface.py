8ad4eb8f3d3cc6352b81e7704d076bcb516f5c1f
skapila <samkapila95@gmail.com>
2026-01-19 14:29:09 +0530

    Delete FlightController directory

--- FlightController/uav_stabilize_controller/interfaces/pid_interface.py
+++
@@ -1,6 +0,0 @@
-from abc import ABC, abstractmethod
-
-class PIDInterface(ABC):
-    @abstractmethod
-    def compute(self, target: float, current: float, dt: float) -> float:
-        pass
--- FlightController/uav_stabilize_controller/sensors/gyro.py
+++
@@ -1,4 +0,0 @@
-class Gyro:
-    def read_roll_rate(self):
-        # Mock roll rate in deg/sec
-        return 0.0
