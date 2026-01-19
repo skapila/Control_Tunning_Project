b6eb6c3bdf692a235a1a2ddd9f1307f5cee715af
samarth <samkapila95@gmail.com>
2026-01-19 14:03:55 +0530

    Removing extra folder

--- FlightController/uav_stabilize_controller/core/flight_mode.py
+++
@@ -1,6 +0,0 @@
-from abc import ABC, abstractmethod
-
-class FlightMode(ABC):
-    @abstractmethod
-    def update(self, pilot_input, dt):
-        pass
--- FlightController/uav_stabilize_controller/sensors/accelerometer.py
+++
@@ -1,4 +0,0 @@
-class Accelerometer:
-    def read_roll(self):
-        # Mock actual roll angle in degrees
-        return 0.0
